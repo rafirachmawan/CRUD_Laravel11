417065bc54bb34ed0cd3837fe7ed56de28ecd92a
Rafi Rachmawan <rafirachmawan1987@gmail.com>
2025-01-10 10:19:30 +0700

    Update Konsep Blade

---
+++ resources/views/coba/halo.blade.php
@@ -0,0 +1,1 @@
+Halo Saya Dari Blade
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Mencoba Routes 
+Route::get('/Halo', function (){
+    return view ('coba.halo');
+});
