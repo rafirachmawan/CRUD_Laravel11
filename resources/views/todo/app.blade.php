3e826557cf11b01a467aeb384a6a9a2bbcf2debe
Rafi Rachmawan <rafirachmawan1987@gmail.com>
2025-01-11 15:28:58 +0700

    Update Tandai Task Yang Sudah Selesai

--- resources/views/todo/app.blade.php
+++ resources/views/todo/app.blade.php
@@ -89,7 +89,11 @@
                            
                             <!-- 04. Display Data -->
                             <li class="list-group-item d-flex justify-content-between align-items-center">
-                                <span class="task-text">{{ $item->task }}</span>
+                                <span class="task-text">
+                                    {!! $item->is_done == '1'?'<del>':'' !!}
+                                    {{ $item->task }}
+                                    {!! $item->is_done == '1'?'</del>':'' !!}
+                                    </span>
                                 <input type="text" class="form-control edit-input" style="display: none;"
                                     value="{{ $item->task }}">
                                 <div class="btn-group">
